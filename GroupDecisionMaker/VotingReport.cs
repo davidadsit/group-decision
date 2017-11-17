c04bb8fdeaf5d281e4a4af3c0bc87ce178878219
davidadsit <david.adsit@gmail.com>
2017-11-17 16:46:02 -0700

    clean up the reporting

--- GroupDecisionMaker/VotingReport.cs
+++ GroupDecisionMaker/VotingReport.cs
@@ -10,23 +10,27 @@ namespace GroupDecisionMaker
         readonly List<Tuple<string, int>> candidates = new List<Tuple<string, int>>();
         readonly StringBuilder textReport;
 
-        public VotingReport()
+        public VotingReport(int totalBallots)
         {
+            TotalBallots = totalBallots;
             textReport = new StringBuilder();
         }
 
         public string Winner { get; set; }
+        public int TotalBallots { get; }
 
         public string GetTextReportText()
         {
             var candidateReportBuilder = new StringBuilder();
-            candidateReportBuilder.AppendLine($"{candidates.Sum(x => x.Item2)} votes were recorded");
+            candidateReportBuilder.AppendLine($"{TotalBallots} ballots were submitted");
+            candidateReportBuilder.AppendLine($" - {candidates.Sum(x => x.Item2)} ballots were used for the final selection");
+            candidateReportBuilder.AppendLine($" - {TotalBallots - candidates.Sum(x => x.Item2)} ballots were exhausted");
             foreach (var candidate in candidates.OrderByDescending(x => x.Item2).ThenBy(x => x.Item1))
             {
                 var vote = candidate.Item2 == 1 ? "vote" : "votes";
                 candidateReportBuilder.AppendLine($"{candidate.Item1} had {candidate.Item2} {vote}");
             }
-            return $"{Winner} wins!{Environment.NewLine}{textReport}{Environment.NewLine}{candidateReportBuilder}";
+            return $"{Winner} wins!{Environment.NewLine}{Environment.NewLine}{textReport}{Environment.NewLine}{Environment.NewLine}{candidateReportBuilder}";
         }
 
         public void AppendLine(string reportLine)
